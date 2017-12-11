9610fb4019381a2c6978b7149a76e5330b0b4e3f
hqh546020152 <546020152@qq.com>
2017-12-11 17:50:39 +0800

    X zabbix_agent.sh

--- zabbix_agent.sh
+++ zabbix_agent.sh
@@ -2,6 +2,22 @@
 #
 
 
+
+cicd_git(){
+
+	git --version  &> /dev/null
+
+	[ $? -eq 0 ] && echo "git已安装完毕" && return 0
+
+	yum install -y git
+	
+	git clone https://github.com/hqh546020152/zabbix_agent.git
+
+	cd zabbix_agent
+	
+	sh zabbix_agent.sh
+}
+
 rpm -ivh http://repo.zabbix.com/zabbix/3.2/rhel/7/x86_64/zabbix-release-3.2-1.el7.noarch.rpm
 
 zabbix_agentd_install(){
@@ -19,3 +35,6 @@ systemctl enable zabbix-agent
 
 #安装zabbix客户端
 zabbix_agentd_install
+#安装git
+
+cicd_git
